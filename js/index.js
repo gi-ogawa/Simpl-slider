15c80f02c683674c2f829bc34b659a9905b90249
gi-ogawa <ogawa_h@gp1000.jp>
2025-07-08 17:02:34 +0900

    jsのコードの一部を追加

---
+++ js/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+  const images = ['/画像/Number-logo-simple-57.png','/画像/Number-logo-simple-58.png','/画像/Number-logo-simple-59.png','/画像/Number-logo-simple-60.png','/画像/Number-logo-simple-61.png',]
+  const slideImg = document.getElementById('slide-img');
+  const prev = document.getElementById('prev');
+  const page = document.getElementById('page');
+  const next = document.getElementById('next');
+
+
+
+}
