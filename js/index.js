0ccd9f0894a40b105161982ae091f63759327e76
gi-ogawa <ogawa_h@gp1000.jp>
2025-07-09 13:43:56 +0900

    カウントのコードを追加

--- js/index.js
+++ js/index.js
@@ -6,7 +6,23 @@
   const prev = document.getElementById('prev');
   const page = document.getElementById('page');
   const next = document.getElementById('next');
+  let current = 0;
 
+  page.textContent = `1/${images.length}`
 
+  next.addEventListener('click',function() {
+    if( current + 1 < images.length) {
+      current++;
+      slideImg.src = images[current];
+    }
+    page.textContent = `${current + 1}/${images.length}`
+  });
 
+  prev.addEventListener('click',function() {
+    if( current > 0) {
+      current--;
+      slideImg.src = images[current];
+    }
+    page.textContent = `${current + 1}/${images.length}`
+  });
 }
